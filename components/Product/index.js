f47ef307edd8a52956a4240ac741f4b3b0c2b0f0
Xaver Schüll <botacode@gmail.com>
2023-04-11 17:55:51 +0200

    More review data are now displayed

--- components/Product/index.js
+++ components/Product/index.js
@@ -9,6 +9,8 @@ export default function Product() {
 
   const { data } = useSWR(id ? `/api/products/${id}` : null);
 
+  console.log("data in Product:", data);
+
   if (!data) {
     return <h1>Loading...</h1>;
   }
@@ -20,6 +22,20 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      {data.reviews.length > 0 ? (
+        <div>
+          <h3>Reviews</h3>
+          {data.reviews.map((review) => (
+            <div key={review._id}>
+              <h4>{review.title}</h4>
+              <p>{review.text}</p>
+              <span>Rated {review.rating}/5</span>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h3>No reviews yet!</h3>
+      )}
       <StyledButton type="button" onClick={() => router.push("/")}>
         Back to all
       </StyledButton>
